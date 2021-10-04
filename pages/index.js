c1c3735dabf2ac20d2acd236917535ce022c6357
Nirawit Jittipairoj <sixfingeredamish@gmail.com>
2021-10-04 20:32:36 +0700

    Add color check

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ export default function Home() {
     <div className="mx-auto w-[300px] mt-[60px]">
       <h1 className="font-semibold text-primary">hello world</h1>
       <p>this is a paragraph</p>
+      <p className="text-placeholder">placeholder</p>
     </div>
   )
 }
