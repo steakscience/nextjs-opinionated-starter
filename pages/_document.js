611de111b82ee0f0c35c7790260aaab95b02a8b2
Nirawit Jittipairoj <sixfingeredamish@gmail.com>
2021-11-30 14:12:58 +0700

    Add display font

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,13 @@ class MyDocument extends Document {
             type="font/woff2"
             crossOrigin="anonymous"
           />
+          <link
+            rel="preload"
+            href="/fonts/InterDisplay.var.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
         </Head>
         <body className="font-sans bg-background-app text-base text-body">
           <Main />
